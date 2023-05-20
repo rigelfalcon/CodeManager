c648169442c8a38656487470e2c2063619492110
rigelfalcon <wy0576zg@gmail.com>
2023-05-20 18:40:55 +0800

    update

--- demo_copy_dependency.m
+++ demo_copy_dependency.m
@@ -1,13 +1,19 @@
 
 %% backup before running, aviod irreversible damage
-srcFile='src\mfile\path\filename.m';
-destPath='path\to\the\dest\code\root';
-srcRoot='path\to\the\src\code\root';
+% srcFile='D:\code\GAM\RelGAMM\external\nufftkreg\example\demo_heteroscedasticity_1d_example.m';
+% destPath='D:\code\GAM\tools\nerips_fkreg\FKreg\';
+% srcRoot='D:\code\GAM\RelGAMM\external\nufftkreg\';
+% copy_dependency(destPath,srcFile,srcRoot);
+
+
+
+
+% srcFile='D:\code\HOA\XiAlphaHOSA\demo\step1_get_channel_info_2420.m';
+% srcFile='D:\code\HOA\XiAlphaHOSA\demo\step2_get_channel_info_1772.m';
+srcFile='D:\code\HOA\XiAlphaHOSA\demo\step3_find_miss_matched_channel_1772_2420.m';
+destPath='D:\code\HOA\iEEGChannelMatch\';
+srcRoot='D:\code\HOA\XiAlphaHOSA\';
 copy_dependency(destPath,srcFile,srcRoot);
-
-
-
-
 
 
 
